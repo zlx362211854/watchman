8ec1a5365465fd8bd42c3a74257415c6195c9733
zlx <zlx2018@aliyun.com>
2022-02-25 20:27:49 +0800

    arguments

--- bin/index.js
+++ bin/index.js
@@ -2,9 +2,12 @@
 var child_process=require("child_process");
 const os = require('os');
 var path = require('path')
+var argv = require('minimist')(process.argv.slice(2));
+var sourceDir = argv._[0] || ""
+var targetDir = argv._[1] || ""
 if (os.type() == 'Windows_NT') {
     //windows
-    child_process.execSync('start ' + path.resolve(__dirname + '/watch.exe'),{stdio: 'inherit'})
+    child_process.execSync('start ' + path.resolve(__dirname + `/watch.exe ${sourceDir} ${targetDir}`))
 } else {
-    child_process.execSync(path.resolve(__dirname + '/watch'),{stdio: 'inherit'})
+    child_process.execSync(path.resolve(__dirname + `/watch ${sourceDir} ${targetDir}`))
 }
